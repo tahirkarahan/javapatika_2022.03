0603380c16f2748c8a7f9e899260e658563cf536
tahir karahan <tahirkarahanli99@gmail.com>
2022-03-20 13:04:14 +0300

    second 3- project 2022-03-20

---
+++ src/main/java/com/patika/java101/CalculateAreaOfThreetriangle.java
@@ -0,0 +1,20 @@
+package com.patika.java101;
+
+import java.util.Scanner;
+
+public class CalculateAreaOfThreetriangle {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter a : ");
+        a = input.nextInt();
+        System.out.print("please enter b : ");
+        b = input.nextInt();
+        System.out.print("please enter c : ");
+        c = input.nextInt();
+        double u = (a + b + c) / 2;
+        System.out.println("perimeter of three-triangle:  " + 2 * u);
+        double s = u * (u - a) * (u - b) * (u - c);
+        System.out.println("area of three-triangle: " + s);
+    }
+}
