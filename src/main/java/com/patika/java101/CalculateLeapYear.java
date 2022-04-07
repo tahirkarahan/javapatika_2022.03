08250f4f2504036a55dce7965a7855d355c0879a
tahir karahan <tahirkarahanli99@gmail.com>
2022-04-07 10:46:43 +0300

    15-project 2022-04-06 finished.

---
+++ src/main/java/com/patika/java101/CalculateLeapYear.java
@@ -0,0 +1,37 @@
+package com.patika.java101;
+
+import java.util.Scanner;
+
+public class CalculateLeapYear {
+    public static void main(String[] args) {
+        //A leap year is a year with 366 days instead of 365 in the Gregorian calendar.
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter a year: ");
+        year = input.nextInt();
+        //1-way
+        if (year > 0) {
+            if (year % 4 == 0) {
+                System.out.printf("%d is a leap year\n", year);
+            } else {
+                System.out.printf("%d is not a leap year\n", year);
+            }
+        } else {
+            System.out.println("you entered wrong number!!");
+        }
+
+        //2-way
+        boolean isArti = false;
+        if (year % 4 == 0) {
+            if (year % 400 == 0) {
+                isArti = true;
+            } else
+                isArti = true;
+        }
+
+        if (isArti)
+            System.out.println(year + " is a leap year !");
+        else
+            System.out.println(year + " is not a leap year !");
+    }
+}
