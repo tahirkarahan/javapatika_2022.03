48184b0aa2d608798be3bd0b8357566a9841e0d4
tahir karahan <tahirkarahanli99@gmail.com>
2022-04-07 10:00:41 +0300

    second 14-project 2022-04-06 finished.

--- src/main/java/com/patika/java101/CalculateChineseZodiac.java
+++ src/main/java/com/patika/java101/CalculateChineseZodiac.java
@@ -21,26 +21,30 @@ Date of Birth 12% = 10 ➜ Horse
 Date of Birth 12% = 11 ➜ Sheep
 */
         int date, remainder;
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Please enter date of birth: ");
-        date=input.nextInt();
-        remainder=date%12;
-        String result = switch (remainder) {
-            case 0 -> ("Monkey");
-            case 1 -> ("Rooster");
-            case 2 -> ("Dog");
-            case 3 -> ("Pig");
-            case 4 -> ("Mice");
-            case 5 -> ("Ox");
-            case 6 -> ("Tiger");
-            case 7 -> ("Rabbit");
-            case 8 -> ("Dragon");
-            case 9 -> ("Snake");
-            case 10 -> ("Horse");
-            case 11 -> ("Sheep");
-            default -> ("you enter wrong number");
-        };
-        System.out.printf("%d / 12 = %d\n",date,remainder);
-        System.out.println("Your chinese zodiac is "+result);
+        date = input.nextInt();
+        remainder = date % 12;
+        if (date > 0) {
+            String result = switch (remainder) {
+                case 0 -> ("Monkey");
+                case 1 -> ("Rooster");
+                case 2 -> ("Dog");
+                case 3 -> ("Pig");
+                case 4 -> ("Mice");
+                case 5 -> ("Ox");
+                case 6 -> ("Tiger");
+                case 7 -> ("Rabbit");
+                case 8 -> ("Dragon");
+                case 9 -> ("Snake");
+                case 10 -> ("Horse");
+                case 11 -> ("Sheep");
+                default -> " ";
+            };
+            System.out.printf("%d / 12 = %d\n", date, remainder);
+            System.out.println("your chinese zodiac is " +result);
+        } else
+            System.out.println("Birth date should be big than 0");
+
     }
 }
